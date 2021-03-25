05aef113e9b8b37895a7fab07864b411b778edb3
Harald Sagar Vöhringer <harald@Haralds-MacBook-Pro.local>
2021-03-25 06:36:05 +0000

    fixing multilineage model

--- genomicsurveillance/models/models.py
+++ genomicsurveillance/models/models.py
@@ -61,7 +61,7 @@ class MultiLineage(Model):
     :param lineages_date:
     :param population: An array indicating the population in each location.
     :param regions: A list of index arrays indicating higher order regions.
-    :param basis: The spline basis function an its derivative (2, time, self.num_basis).
+    :param basis: The spline basis function an its derivative (2, time, num_basis).
     :param tau: Generation time in days.
     :param init_scale: Scaling factor of variational distributions
     :param beta_loc: Mean of the spline regression coefficients.
@@ -73,12 +73,12 @@ class MultiLineage(Model):
     :param rho_loc: Mean of the overdispersion parameter, defaults to np.log(10).
     :param rho_scale: Standard deviation of the overdispersion parameter.
     :param multinomial_scale: Weight of the multinomial log likelihood.
-    :param time_scale: Parameter to scale the variance of b0.
+    :param time_scale: Parameter to scale the variance of mu_b.
     :param exclude: Exclude missing lineages during the analysis
     :kwargs: SVI Handler arguments.
     """
 
-    _latent_variables = [Sites.BC0, Sites.C, Sites.BETA]
+    _latent_variables = [Sites.BETA1, Sites.B1, Sites.C1]
 
     def __init__(
         self,
@@ -86,7 +86,6 @@ class MultiLineage(Model):
         lineages: np.ndarray,
         lineage_dates: np.ndarray,
         population: np.ndarray,
-        regions=None,
         basis=None,
         tau: float = 5.0,
         init_scale: float = 0.1,
@@ -95,15 +94,12 @@ class MultiLineage(Model):
         b0_scale: float = 0.2,
         c0_loc: float = -10.0,
         c0_scale: float = 5.0,
-        c_loc: float = 0.0,
         c_scale: float = 10.0,
         fit_rho: bool = False,
         rho_loc: float = np.log(10.0),
         rho_scale: float = 1.0,
-        multinomial_scale: float = 1.0,
         time_scale: float = 100.0,
-        exclude: bool = True,
-        handler: str = "SVI",
+        sample_deterministic: bool = False,
         *args,
         **kwargs,
     ):
@@ -114,12 +110,11 @@ class MultiLineage(Model):
         assert (
             cases.shape[0] == lineages.shape[0]
         ), "cases and lineages must have the number of location"
-        super().__init__(handler, *args, **kwargs)
+        super().__init__(**kwargs)
         self.cases = cases
         self.lineages = lineages
         self.lineage_dates = lineage_dates
         self.population = population
-        self.regions = regions
 
         self.tau = tau
         self.init_scale = init_scale
@@ -133,18 +128,17 @@ class MultiLineage(Model):
         self.c0_loc = c0_loc
         self.c0_scale = c0_scale
 
-        self.c_loc = c_loc
+        self.c_loc = 0.0
         self.c_scale = c_scale
 
         self.rho_loc = rho_loc
         self.rho_scale = rho_scale
-        self.exclude = exclude
 
-        self.multinomial_scale = multinomial_scale
         self.time_scale = time_scale
 
         self.nan_idx = nan_idx(cases, lineages)
         self.missing_lineages = missing_lineages(lineages)[self.nan_idx].astype(int)
+        self.sample_deterministic = sample_deterministic
 
         if basis is None:
             _, self.B = create_spline_basis(
@@ -155,12 +149,11 @@ class MultiLineage(Model):
         else:
             self.B = basis
 
-        # number
         self.num_ltla = self.cases.shape[0]
         self.num_time = self.cases.shape[1]
         self.num_lin = self.lineages.shape[-1] - 1
         self.num_basis = self.B.shape[-1]
-        self.num_ltla_lin = self.nan_idx.shape[0]
+        self.self.num_ltla_lin = self.nan_idx.shape[0]
 
     def model(self):
         """The model."""
@@ -184,7 +177,10 @@ class MultiLineage(Model):
             ),
         )
 
-        beta = expand(beta, index[self.nan_idx, :], (self.num_ltla, self.num_basis))
+        beta = npy.deterministic(
+            Sites.BETA1,
+            expand(beta, index[self.nan_idx, :], (self.num_ltla, self.num_basis)),
+        )
 
         # MVN prior for b and c parameter
         bc0_loc = jnp.concatenate(
@@ -196,7 +192,7 @@ class MultiLineage(Model):
         bc0_scale = jnp.diag(
             jnp.concatenate(
                 [
-                    jnp.repeat(self.time_scale * self.b0_scale, self.num_lin),
+                    self.time_scale * jnp.repeat(self.b0_scale, self.num_lin),
                     jnp.repeat(self.c0_scale, self.num_lin),
                 ]
             )
@@ -206,7 +202,7 @@ class MultiLineage(Model):
         # split the array
         b = bc0[: self.num_lin] / self.time_scale
 
-        # offset terms
+        # sample non-centered c
         c0 = bc0[self.num_lin :]
         c_offset = npy.sample(
             Sites.C,
@@ -215,30 +211,41 @@ class MultiLineage(Model):
                 jnp.tile(self.c_scale, (self.num_ltla_lin, self.num_lin)),
             ),
         )
-
         c = c_offset + c0
 
-        b1 = pad(
-            expand(
-                (self.missing_lineages * b).reshape(self.num_ltla_lin, 1, -1),
-                index[self.nan_idx, :, :],
-                (self.num_ltla, 1, self.num_lin),
-            )
+        b1 = npy.deterministic(
+            Sites.B1,
+            pad(
+                expand(
+                    (self.missing_lineages * b).reshape(self.num_ltla_lin, 1, -1),
+                    index[self.nan_idx, :, :],
+                    (self.num_ltla, 1, self.num_lin),
+                )
+            ),
         )
-        c1 = pad(
-            expand(
-                (self.missing_lineages * c).reshape(self.num_ltla_lin, 1, -1),
-                index[self.nan_idx, :, :],
-                (self.num_ltla, 1, self.num_lin),
-            )
+        c1 = npy.deterministic(
+            Sites.C1,
+            pad(
+                expand(
+                    c.reshape(self.num_ltla_lin, 1, -1),
+                    index[self.nan_idx, :, :],
+                    (self.num_ltla, 1, self.num_lin),
+                )
+            ),
         )
 
         # Lineage specific regression coefficients (self.num_ltla x self.num_basis x self.num_lin)
         logits = b1 * jnp.arange(self.num_time).reshape(1, -1, 1) + c1
         p = jnp.exp(logits) / jnp.exp(logsumexp(logits, -1, keepdims=True))
 
-        mu = beta @ self.B[0].T
-        lamb = self.population.reshape(-1, 1) * jnp.exp(mu)
+        if self.sample_deterministic:
+            npy.deterministic(Sites.P, p)
+
+        mu = jnp.exp(beta @ self.B[0].T)
+        lamb = self.population.reshape(-1, 1) * mu
+
+        if self.sample_deterministic:
+            npy.deterministic(Sites.LAMBDA, lamb)
 
         npy.sample(
             Sites.CASES,
@@ -249,23 +256,27 @@ class MultiLineage(Model):
         # with lineage_context:
         npy.sample(
             Sites.LINEAGE,
-            MultinomialProbs(
+            dist.MultinomialProbs(
                 p[self.nan_idx][:, self.lineage_dates],
                 total_count=self.lineages[self.nan_idx].sum(-1),
-                scale=self.multinomial_scale,
             ),
             obs=self.lineages[self.nan_idx],
         )
+        if self.sample_deterministic:
+            npy.deterministic(
+                Sites.LAMBDA_LINEAGE,
+                self.population.reshape(-1, 1, 1) * mu[..., jnp.newaxis] * p,
+            )
 
     def guide(self):
         if self.fit_rho:
             rho_loc = npy.param(
                 Sites.RHO + Sites.LOC,
-                jnp.tile(self.rho_loc, (self.num_ltla, 1)),
+                self.rho_loc * jnp.ones((self.num_ltla, 1)),
             )
             rho_scale = npy.param(
                 Sites.RHO + Sites.SCALE,
-                jnp.tile(self.init_scale * self.rho_scale, (self.num_ltla, 1)),
+                self.init_scale * self.rho_scale * jnp.ones((self.num_ltla, 1)),
                 constraint=dist.constraints.positive,
             )
             npy.sample(Sites.RHO, dist.Normal(rho_loc, rho_scale))
@@ -283,14 +294,17 @@ class MultiLineage(Model):
             constraint=dist.constraints.lower_cholesky,
         )
 
-        npy.sample(Sites.BETA, dist.MultivariateNormal(beta_loc, scale_tril=beta_scale))
+        # cov = jnp.matmul(β_σ, jnp.transpose(β_σ, (0, 2, 1)))
+        npy.sample(
+            Sites.BETA, dist.MultivariateNormal(beta_loc, scale_tril=beta_scale)
+        )  # cov
 
         bc0_loc = npy.param(
             Sites.BC0 + Sites.LOC,
             jnp.concatenate(
                 [
-                    jnp.repeat(self.b0_loc, self.num_lin),
-                    jnp.repeat(self.c0_loc, self.num_lin),
+                    self.b0_loc * jnp.ones(self.num_lin),
+                    self.c0_loc * jnp.ones(self.num_lin),
                 ]
             ),
         )
@@ -299,25 +313,28 @@ class MultiLineage(Model):
             jnp.diag(
                 jnp.concatenate(
                     [
-                        jnp.repeat(
-                            self.init_scale * self.b0_scale * self.time_scale,
-                            self.num_lin,
-                        ),
-                        jnp.repeat(self.init_scale * self.c0_scale, self.num_lin),
+                        self.init_scale
+                        * self.b0_scale
+                        * self.time_scale
+                        * jnp.ones(self.num_lin),
+                        self.init_scale * self.c0_scale * jnp.ones(self.num_lin),
                     ]
                 )
-            ),
+            ).reshape(2 * self.num_lin, 2 * self.num_lin),
             constraint=dist.constraints.lower_cholesky,
         )
         npy.sample(Sites.BC0, dist.MultivariateNormal(bc0_loc, scale_tril=bc0_scale))
 
         c_loc = npy.param(
-            Sites.C + Sites.LOC, jnp.tile(self.c_loc, (self.num_ltla_lin, self.num_lin))
+            Sites.C + Sites.LOC,
+            self.c_loc * jnp.ones((self.num_ltla_lin, self.num_lin)),
         )
-
         c_scale = npy.param(
             Sites.C + Sites.SCALE,
-            jnp.tile(self.init_scale * self.c_scale, (self.num_ltla_lin, self.num_lin)),
+            self.init_scale
+            * self.c_scale
+            * jnp.ones((self.num_ltla_lin, self.num_lin)),
+            constraint=dist.constraints.positive,
         )
         npy.sample(Sites.C, dist.Normal(c_loc, c_scale))
 
--- genomicsurveillance/models/sites.py
+++ genomicsurveillance/models/sites.py
@@ -14,11 +14,17 @@ class Sites:
 
     A0 = "a0"
     B0 = "b0"
+    C0 = "c0"
     BC0 = "bc0"
 
     A = "a"
     B = "b"
     C = "c"
+
+    A1 = "a1"
+    B1 = "b1"
+    C1 = "c1"
+    BETA1 = "beta1"
 
     MU = "mu"
 
